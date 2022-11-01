2d7585bf7139e6e68feafd66999dc38825d086ba
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2022-11-01 19:42:32 +0000

    Add input name funtion to the game.

--- run.py
+++ run.py
@@ -3,3 +3,6 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 print("This is a game of Battleships")
+
+insert_name = input("Enter your name here: \n")
+print(f"Hi {insert_name}, get ready to play BattleShips!")
