1ac8fc99fa479a81be52cbaa26620697ba2793c0
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2022-11-01 21:38:34 +0000

    Set game board size rows to 6.

--- run.py
+++ run.py
@@ -6,3 +6,12 @@ print("This is a game of Battleships")
 
 insert_name = input("Enter your name here: \n")
 print(f"Hi {insert_name}, get ready to play BattleShips!")
+        
+#board set up
+game_board = []
+
+for x in range(0, 6):
+    game_board.append(["0"] * 6)
+    print(game_board)
+
+
