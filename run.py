1e4a56a31e9aea690cb76e5d9dba413f2d4989e5
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2022-11-11 18:01:06 +0000

    Increase turns_left to 20. And delete duplicate your coordinates statement.

--- run.py
+++ run.py
@@ -3,11 +3,6 @@ import enum
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-print("This is a game of Battleships")
-# insert name to personalise user board name
-insert_name = input("Enter your name here: \n")
-print(f"Hi {insert_name}, get ready to play BattleShips!")
-print('\n')
 
 BOARD_GRID_SIZE = 6
 NO_OF_SHIPS = 5
@@ -24,8 +19,8 @@ USER_BOARD = [[BoardStates.EMPTY.value] * BOARD_GRID_SIZE for x in range(BOARD_G
 #COMPUTERS_BOARD = [[" "] * BOARD_GRID_SIZE for y in range(BOARD_GRID_SIZE)]
 # .replace and . split are not working to remove comma so used this method instead
 # function to see the board
-def print_game_board(game_board):
-    print(f"{insert_name}'s BOARD:")
+def print_game_board(user_name, game_board):
+    print(f"{user_name}'s BOARD:")
     print('   0 1 2 3 4 5')
     print('---------------')
     row_num = 0
@@ -36,8 +31,8 @@ def print_game_board(game_board):
         #print("%d |%s|" % (row_num, "|".join(row)))
         print("%d |%s|" % (row_num, row_to_be_printed))
         row_num += 1
-print_game_board(USER_BOARD)
-print("\n")
+#print_game_board(USER_BOARD)
+#print("\n")
 
 #def print_computer_board(game_board):
  #   print('COMPUTER BOARD: ')
@@ -80,7 +75,7 @@ def user_input():
         print("Row coordinate not valid, try again")
         user_guess_row = input(("Enter your column here: "))
     print(f"Your coordinate: [{user_guess_row} , {user_guess_column}]")
-    print(f"Your coordinate: [{user_guess_column} , {user_guess_row}]")
+    #print(f"Your coordinate: [{user_guess_column} , {user_guess_row}]")
     return int(user_guess_row), int(user_guess_column)
 
 
@@ -89,12 +84,17 @@ def user_input():
 #print_game_board(COMPUTERS_BOARD)
 def start_game():
     user_score = 0
-    turns_left = 15
+    turns_left = 20
+    print("This is a game of Battleships")
+    # insert name to personalise user board name
+    user_name = input("Enter your name here: \n")
+    print(f"Hi {user_name}, get ready to play BattleShips!")
+    print('\n')
 
     computers_ships(USER_BOARD)
     while turns_left > 0 or user_score < 5:
         print("Welcome to the game")
-        print_game_board(USER_BOARD)
+        print_game_board(user_name, USER_BOARD)
         user_guess_column, user_guess_row = user_input()
         print('\n')
         print(USER_BOARD[user_guess_column][user_guess_row])
@@ -112,13 +112,13 @@ def start_game():
             turns_left -= 1
         if user_score == 5:
             print("Congrats! you sunk all 5 ships and have won the game!")
-            print_game_board(USER_BOARD)
+            print_game_board(user_name, USER_BOARD)
             break
         print(f"User Score: {user_score}")
         print(f"You have {turns_left} turns left")
         if turns_left == 0:
             print("No more turns left! GAME OVER")
-            print_game_board(USER_BOARD)
+            print_game_board(user_name, USER_BOARD)
             break
 
 
