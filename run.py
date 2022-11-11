a55ce4ec1b6b2a2694825ee17c77089037f7c6bc
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2022-11-11 16:00:22 +0000

    Import enum to make enumerations for different board symbols.

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from random import randint
+import enum
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
