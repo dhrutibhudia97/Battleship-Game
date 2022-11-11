99248685a1a4002b4664133c6b76c8493420dfee
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2022-11-11 11:23:54 +0000

    Removed rest of comments and added print board function to end of game.

--- run.py
+++ run.py
@@ -49,23 +49,26 @@ print_computer_board(COMPUTERS_BOARD)
 #get computer random generated ship location
 def computers_ships(game_board):
     for target in range(5):
-        target_row, target_col = randint(0, 5), randint(0, 5)
+        target_row = randint(0, 5)
+        target_col = randint(0, 5)
         while game_board[target_row][target_col] == "X":
-            target_row, target_col = randint(0, 5), randint(0, 5)
+            target_row = randint(0, 5)
+            target_col = randint(0, 5)
 	        # target_row, target_col = user_input()
         game_board[target_row][target_col] = "X" 
 
 
 # NEED TO ADD THESE AS VALIDITY CHECKERS
+# user needs to input data
 # users guess is stored so they can't guess same numbers again
-# invalid user input/error options if (1)Not an integer
+# invalid user input/error options if (1)Not an integer (is.digit...?)
 # elif (2) Integer not in range. else(3)Guessed that integer combo already
 
 # print user prompt message
 print("\nCoordinates should be 2 numbers (between 0 - 5) seperated by a comma.")
 print("Example:[3, 5] \n")
 
-# user_guess = (f"[{input('Enter your row here: ')} , {input('Enter your column here: ')}]")
+
 # asks users what row and column to guess battleship is in
 def user_input():
     user_guess_column = input("Enter your row here: ")
@@ -78,71 +81,10 @@ def user_input():
         user_guess_row = input(("Enter your column here: "))
     print(f"Your coordinate: [{user_guess_row} , {user_guess_column}]")
     return int(user_guess_row), int(user_guess_column)
-    #NEED TO ADD VALIDITY CHECK TO UNPUT LATER... INTEGER OF CORRECT RANGE
-    # THATS not been repeated before ALSO not a blank!
-
-    #if (
-    # user_guess_row.isdigit() and user_guess_column.isdigit()):  
-        # tried adding and in range(5)... didn't work
-     #   print("Provided value is an integer")
-      #  while user_guess_row > 6:
-       #     print("Please enter valid row number between 0 - 5")
-        #    user_guess_row = input("Enter your row here: ")
-    #    while user_guess_column > 6:
-     #       print("Please enter valid column number between 0 - 5")
-      #      user_guess_column = input("Enter your column here: ")
-       # return int(user_guess_row), int(user_guess_column)
-        #print(f"{user_guess_row}, {user_guess_column}")
-
-#user_input()
-    # if int(user_guess_row) > 5 and int(user_guess_column) > 5:
-    #    print("number is not on the grid")
-    # user_guess = int(integer)
-    # print(user_guess)
-    #if (user_guess_row) or (user_guess_column): #> not supported between str and int
-     #   print("That coordinate is not on the grid.")
-#else:
-#    print("Provided value is not an integer")
-    # continue not properly in loop
-
-#   try:
-#      [int(value) for value in values]
-#     if len(values) != 2:
-#        raise ValueError(
-#          f"Exactly 6 values required, you provided {len(values)}"
-#       )
-#      elif
-# except ValueError as e:
-#   print(f"Invalid data: {e}, please try again.\n")
-#  return False
-
-# return True
-
-#if user_guess_row == computer_ship_row and user_guess_column == computer_ship_col:
- #   print("Congratulations you hit my battleship :D")
-#else:
- #   print("Aww, you missed my battleship! :(")
-   # USER_BOARD[int(user_guess_row)][int(user_guess_column)] = "X"
-  #  print_game_board(USER_BOARD)
 
 
-#score tally
-#def user_score(game_board):
- #   score = 0
-  #  for row in game_board:
-   #     for column in row:
-    #        if column == "X":
-     #           score += 1
-   # return score
-    #if score == 5:
-     #   print(YOU HAVE DEFEATED ALL MY BATTLESHIPS! CONGRATS!)
-     #   break
-      #  else:
-       #     continue
 
-
-# turns left to guess on computers hidden board
-
+# game function, only runs if there are user turns left and ships left to hit.
 computers_ships(COMPUTERS_BOARD)
 print_game_board(COMPUTERS_BOARD)
 user_score = 0
@@ -164,32 +106,12 @@ while turns_left > 0 or user_score < 5:
         turns_left -= 1
     if user_score == 5:
         print("Congrats! you sunk all 5 ships and have won the game!")
+        print_game_board(USER_BOARD)
         break
     print(f"User Score: {user_score}")
     print(f"You have {turns_left} turns left")
     if turns_left == 0:
         print("No more turns left! GAME OVER")
+        print_game_board(USER_BOARD)
         break
 
-
-
-
-#print(COMPUTERS_BOARD)
-#print(USER_BOARD)
-
-# similar to switch case... get user results...
-# if computer_guess == user guess
-#   print("congrats... you hit s ship")
-#   user score = += 1
-#   user_goes_left -=1
-
-# elif computer guess == previous user guess
-# print (you have already hit this spot...)
-
-# elif user score == 5
-# print (congrats, you blew up all of their ships...you win)
-# break out of loop.
-
-# else computer guess != user guess
-#     print("YOU MISSED")
-#     user_goes_left -=1
